aae736785c0726bb17ec411fd4160b42ce2235fa
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-07 15:38:49 +0200

    Add new blank fragment: AddExpenseFragment

---
+++ app/src/main/java/no/hiof/trondkw/AddExpenseFragment.java
@@ -0,0 +1,30 @@
+package no.hiof.trondkw;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddExpenseFragment extends Fragment {
+
+    public AddExpenseFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_add_expense, container, false);
+    }
+}
