577d4bec233a08bcd563a64e481ff678b384d9ba
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-07 13:17:10 +0200

    Add new blank fragment: MainOverviewFragment

---
+++ app/src/main/java/no/hiof/trondkw/MainOverviewFragment.java
@@ -0,0 +1,28 @@
+package no.hiof.trondkw;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainOverviewFragment extends Fragment {
+
+    public MainOverviewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main_overview, container, false);
+    }
+}
