84e662951003509585c82b437b4fcdebf31cd2d9
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-15 18:07:12 +0200

    Add working DatePickerDialog to AddExpenseFragment to select date for expense.

--- app/src/main/java/no/hiof/trondkw/AddExpenseFragment.java
+++ app/src/main/java/no/hiof/trondkw/AddExpenseFragment.java
@@ -1,5 +1,6 @@
 package no.hiof.trondkw;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,16 +13,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
-public class AddExpenseFragment extends Fragment {
+public class AddExpenseFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private TextView dateEditText;
+
+    private int dayOfMonth;
+    private int month;
+    private int year;
+
 
     public AddExpenseFragment() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,9 @@ public class AddExpenseFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dateEditText = view.findViewById(R.id.AddExpenseFragment_SelectDateView);
+        dateEditText.setOnClickListener(view1 -> showDatePickerDialog());
+        setDefaultDate();
 
         // SELECT EXPENSE CATEGORY
         // populate category spinner with a predefined string array
@@ -49,7 +63,29 @@ public class AddExpenseFragment extends Fragment {
     }
 
 
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), this, year, month, dayOfMonth);
+
+        datePickerDialog.show();
+    }
 
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.year = year;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
+        String date = "day/month/year: " + dayOfMonth + "/" + (month + 1) + "/" + year;
+        dateEditText.setText(date);
+    }
+
+    private void setDefaultDate() {
+        year = Calendar.getInstance().get(Calendar.YEAR);
+        month = Calendar.getInstance().get(Calendar.MONTH);
+        dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        String date = "Today";
+        dateEditText.setText(date);
+    }
 
 }
