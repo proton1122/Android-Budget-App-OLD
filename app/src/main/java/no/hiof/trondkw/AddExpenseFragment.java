61ed5d15b7adab4b38c6e83ca9a77583b0ce66b4
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-15 17:15:39 +0200

    Add dropdown list for categories with predefined string array of categories.

--- app/src/main/java/no/hiof/trondkw/AddExpenseFragment.java
+++ app/src/main/java/no/hiof/trondkw/AddExpenseFragment.java
@@ -2,11 +2,17 @@ package no.hiof.trondkw;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
 public class AddExpenseFragment extends Fragment {
@@ -24,7 +30,26 @@ public class AddExpenseFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_add_expense, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        // SELECT EXPENSE CATEGORY
+        // populate category spinner with a predefined string array
+        // TODO
+        // Separate class / data for categories
+        Spinner spinner = view.findViewById(R.id.AddExpenseFragment_SelectCategorySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.expense_category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+
+
+
 }
