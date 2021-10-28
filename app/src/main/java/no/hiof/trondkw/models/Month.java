4ec2d76e6a7072d290e577d44bb9144b79a34cb8
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-28 15:43:19 +0200

    Add getters / setters for Month class

--- app/src/main/java/no/hiof/trondkw/models/Month.java
+++ app/src/main/java/no/hiof/trondkw/models/Month.java
@@ -25,11 +25,55 @@ public class Month {
     // -------------------------------------------------------------------
     // constructors
     public Month() {
-
+        this.budget = 0;
+        this.expenses = 0;
+        this.remaining = 0;
     }
     // -------------------------------------------------------------------
 
     // TODO
     //
 
+
+
+    // -------------------------------------------------------------------
+    // getters / setters
+
+    public int getMonthId() {
+        return monthId;
+    }
+
+    public ArrayList<Expense> getMonthlyExpenses() {
+        return monthlyExpenses;
+    }
+    public void setMonthlyExpenses(ArrayList<Expense> monthlyExpenses) {
+        this.monthlyExpenses = monthlyExpenses;
+    } // delete?
+
+    public double getBudget() {
+        return budget;
+    }
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getExpenses() {
+        return expenses;
+    }
+    public void setExpenses(double expenses) {
+        this.expenses = expenses;
+    } // get and add all expenses from ArrayList<Expense>
+
+    public double getRemaining() {
+        return remaining;
+    }
+    public void setRemaining(double remaining) {
+        this.remaining = remaining;
+    } // delete? ( budget - expenses )
+    // -------------------------------------------------------------------
+
+
+
+
+    
 }
