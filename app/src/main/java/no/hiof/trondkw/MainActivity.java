80a71dafb2d099b15a515560d4253b7408f0fbc8
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-10-29 22:59:32 +0200

    Clean up a bit before start working on Data Binding and ViewModel.

--- app/src/main/java/no/hiof/trondkw/MainActivity.java
+++ app/src/main/java/no/hiof/trondkw/MainActivity.java
@@ -1,14 +1,27 @@
 package no.hiof.trondkw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import no.hiof.trondkw.viewmodels.MonthViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MonthViewModel monthViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //monthViewModel = new ViewModelProvider()
+
     }
+
+
+
 }
--- app/src/main/java/no/hiof/trondkw/viewmodels/MonthViewModel.java
+++ app/src/main/java/no/hiof/trondkw/viewmodels/MonthViewModel.java
@@ -1,27 +1,29 @@
 package no.hiof.trondkw.viewmodels;
 
 import android.app.Application;
+import android.content.ClipData;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
+import no.hiof.trondkw.models.Expense;
 import no.hiof.trondkw.models.Month;
 import no.hiof.trondkw.repositories.MonthRepository;
 
-public class MonthViewModel extends AndroidViewModel {
+public class MonthViewModel extends ViewModel {
 
-    private MonthRepository repository;
-    private LiveData<List<Month>> allMonths;
+    //private MonthRepository repository;
 
-    // Application context is needed for database, so this might be unnecessary
-    public MonthViewModel(@NonNull Application application) {
-        super(application);
+    private MutableLiveData<Month> currentMonth;
+    private MutableLiveData<List<Expense>> expenses;
 
-        repository = new MonthRepository(application);
-        allMonths = repository.getAllMonths(); // RETURNS NULL!
+
+    public MonthViewModel() {
 
     }
 
@@ -29,6 +31,21 @@ public class MonthViewModel extends AndroidViewModel {
 
 
 
+
+    // TEST
+
+    //private final MutableLiveData<List<Month>> months = new MutableLiveData<>();
+    //private MutableLiveData<List<Month>> mAllMonths;
+    //private LiveData<List<Month>> allMonths;
+
+    // Application context is needed for database, so this might be unnecessary
+    public MonthViewModel(@NonNull Application application) {
+
+
+        //repository = new MonthRepository(application);
+        //allMonths = repository.getAllMonths(); // RETURNS NULL!
+
+    }
 
     // database methods
 
@@ -41,7 +58,5 @@ public class MonthViewModel extends AndroidViewModel {
     public LiveData<List<Month>> getAllMonths() {
         return null;
     }
-
-
 
 }
