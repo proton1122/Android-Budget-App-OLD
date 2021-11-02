ab5437d167845e128aed1d3836e4f3a5785423fd
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-11-02 19:37:38 +0100

    Trying ViewModel again, but can't get setting values to work.

--- app/src/main/java/no/hiof/trondkw/fragments/MainOverviewFragment.java
+++ app/src/main/java/no/hiof/trondkw/fragments/MainOverviewFragment.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -30,6 +31,7 @@ public class MainOverviewFragment extends Fragment {
 
     // fields
 
+    //private LiveData<Month> currentMonth;
     private Month currentMonth;
 
     private FragmentMainOverviewBinding binding;
@@ -40,7 +42,7 @@ public class MainOverviewFragment extends Fragment {
     // constructor
     public MainOverviewFragment() {
         // Required empty public constructor
-        loadData();
+        //loadData();
     }
 
 
@@ -48,17 +50,17 @@ public class MainOverviewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // get the view model
         monthViewModel = new ViewModelProvider(this).get(MonthViewModel.class);
 
+        /*
         // observe the viewModel
         monthViewModel.getCurrentMonth().observe(this, month -> {
 
             // update UI
         });
-
-
-
+         */
 
     }
 
@@ -68,14 +70,16 @@ public class MainOverviewFragment extends Fragment {
 
         // bind data (to be changed)
         binding.setMonthViewModel(monthViewModel);
+
         //binding.setCurrentMonth(currentMonth);
 
 
         // wrong? which object to observe and where?
+        /*
         binding.getMonthViewModel().getCurrentMonth().observe(getViewLifecycleOwner(), month -> {
 
         });
-
+        */
 
 
         // bind data with ViewModel
@@ -89,26 +93,39 @@ public class MainOverviewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-
-        // load data
-        //loadData();
-
-
-        // show data
-        showData(view);
-
-
-
         // navigation
         binding.MainOverviewFragmentFloatingActionButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainOverviewFragment_to_addExpenseFragment));
         binding.MainOverviewFragmentDetailsButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainOverviewFragment_to_monthlyDetailsFragment));
 
-        //Button goToAddExpenseFragment = view.findViewById(R.id.MainOverviewFragment_AddExpenseButton);
-        //goToAddExpenseFragment.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainOverviewFragment_to_addExpenseFragment));
+        // show data
 
-        //Button goToMonthlyDetailsFragment = view.findViewById(R.id.MainOverviewFragment_DetailsButton);
-        //goToMonthlyDetailsFragment.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainOverviewFragment_to_monthlyDetailsFragment));
+
+        /*
+        monthViewModel.getCurrentMonth().observe(getViewLifecycleOwner(), month -> {
+            binding.MainOverviewFragmentRemainingInputTextView.setText("5000");
+        });
+         */
+
+
+
+
+
+
+        binding.getMonthViewModel().getCurrentMonth().observe(getViewLifecycleOwner(), month -> {
+            binding.MainOverviewFragmentBudgetInputTextView.setText(String.valueOf(monthViewModel.getCurrentMonth().getValue().getBudget()));
+            double exp = calcExpenses(monthViewModel.getCurrentMonth().getValue().getMonthlyExpenses());
+            System.out.println("Observer: " + exp);
+            binding.MainOverviewFragmentExpensesInputTextView.setText(String.valueOf(calcExpenses(monthViewModel.getCurrentMonth().getValue().getMonthlyExpenses())));
+            binding.MainOverviewFragmentRemainingInputTextView.setText("5000");
+        });
+
+
+        //binding.MainOverviewFragmentBudgetInputTextView.setText(String.valueOf(currentMonth.getValue().getBudget()));
+
+        /*
+        loadData();
+        showData(view);
+         */
     }
 
 
@@ -118,39 +135,45 @@ public class MainOverviewFragment extends Fragment {
 
         // temp get dummy data
         currentMonth = MonthRepository.getTestMonth();
+        //currentMonth = monthViewModel.getCurrentMonth();
+
     }
 
 
     private void showData(View view) {
         // graph...
 
-
         // budget
         TextView monthlyBudgetTextView = view.findViewById(R.id.MainOverviewFragment_BudgetInputTextView);
+
+        // changed currentMonth from dummy data Month to LiveData<Month>
+        //monthlyBudgetTextView.setText(String.valueOf(currentMonth.getBudget()));
+
+        // why possible NullPointerException?
         monthlyBudgetTextView.setText(String.valueOf(currentMonth.getBudget()));
+
+
 
         // expenses
         TextView monthlyExpensesTextView = view.findViewById(R.id.MainOverviewFragment_ExpensesInputTextView);
-
         ArrayList<Expense> expenses = currentMonth.getMonthlyExpenses();
+        double totalExpenses = calcExpenses(expenses);
+        monthlyExpensesTextView.setText(String.valueOf(totalExpenses));
 
+        // remaining
+        TextView monthlyRemainingTextView = view.findViewById(R.id.MainOverviewFragment_RemainingInputTextView);
+        double remaining = currentMonth.getBudget() - totalExpenses;
+        monthlyRemainingTextView.setText(String.valueOf(remaining));
+    }
+
+    private double calcExpenses(ArrayList<Expense> expenses) {
         double totalExpenses = 0;
 
         for (Expense expense: expenses) {
             totalExpenses += expense.getSum();
         }
-
-        monthlyExpensesTextView.setText(String.valueOf(totalExpenses));
-
-
-        // remaining
-        TextView monthlyRemainingTextView = view.findViewById(R.id.MainOverviewFragment_RemainingInputTextView);
-
-        double remaining = currentMonth.getBudget() - totalExpenses;
-
-        monthlyRemainingTextView.setText(String.valueOf(remaining));
-
-
+        System.out.println("Total expenses: " + totalExpenses);
+        return totalExpenses;
     }
 
 
--- app/src/main/java/no/hiof/trondkw/fragments/MonthlyDetailsFragment.java
+++ app/src/main/java/no/hiof/trondkw/fragments/MonthlyDetailsFragment.java
@@ -8,16 +8,23 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import no.hiof.trondkw.R;
 import no.hiof.trondkw.adapter.ExpenseRecyclerAdapter;
 import no.hiof.trondkw.models.Expense;
 import no.hiof.trondkw.models.Month;
 import no.hiof.trondkw.repositories.MonthRepository;
+import no.hiof.trondkw.viewmodels.MonthViewModel;
 
 public class MonthlyDetailsFragment extends Fragment {
+
+    private MonthViewModel monthViewModel;
 
 
     public MonthlyDetailsFragment() {
@@ -26,6 +33,9 @@ public class MonthlyDetailsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // get the view model
+        monthViewModel = new ViewModelProvider(this).get(MonthViewModel.class);
     }
 
     @Override
@@ -37,6 +47,12 @@ public class MonthlyDetailsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        Month testMonth = monthViewModel.getCurrentMonth().getValue();
+        ArrayList<Expense> expenses = monthViewModel.getCurrentMonth().getValue().getMonthlyExpenses();
+        System.out.println("OnViewCreated: " + testMonth.getBudget());
+
 
         testSetupRecyclerView(view);
 
